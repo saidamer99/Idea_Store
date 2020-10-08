2d266bff8c0e5ed539e16221df2752464b809a9b
Mayar <mayarmayar886@gmail.com>
2020-10-08 17:30:36 +0300

    mayar add drawer and exclus

--- lib/component/drawer.dart
+++ lib/component/drawer.dart
@@ -1,101 +1,130 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class MyDrawer extends StatelessWidget {
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width - 50,
+      width: MediaQuery.of(context).size.width - 70,
       child: Drawer(
-        child: ListView(
-          children: <Widget>[
-            Directionality(
-                textDirection: TextDirection.ltr,
-                child: UserAccountsDrawerHeader(
-                  accountName: null,
-                  accountEmail: null,
-                  currentAccountPicture: CircleAvatar(
-                      foregroundColor: Colors.black,
-                      child: Center(
-                        child: Icon(
-                          Icons.account_circle,
-                          color: Colors.yellow[400],
-                        ),
-                      ),
-                      backgroundColor: Colors.black12.withOpacity(0.3)),
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("images/said.jpg"),
-                          fit: BoxFit.cover)),
-                )),
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('images/login/wallpaper.jpg'))),
+          child: ListView(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                      iconSize: 60,
+                      color: Colors.red,
+                      icon: Icon(Icons.person),
+                      onPressed: () {}),
+                  Text(
+                    'mayar',
+                    style: TextStyle(color: Colors.red),
+                  )
+                ],
+              ),
+            ),
             ListTile(
               onTap: () {
                 Navigator.of(context).pushNamed('go');
               },
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
               leading: Icon(
                 Icons.home,
-                color: Colors.blue,
+                color: Colors.white,
               ),
               title: Text(
                 "الصفحة الرئيسية",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
             ),
             ListTile(
               onTap: () {
                 Navigator.of(context).pushNamed("Categories");
               },
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
               leading: Icon(
                 Icons.list,
-                color: Colors.blue,
+                color: Colors.white,
               ),
               title: Text(
                 "الأقسام",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
             ),
             Container(
               child: Divider(
-                color: Colors.black,
+                color: Colors.red,
               ),
             ),
             ListTile(
               onTap: () {
                 Navigator.of(context).pushNamed("startFromHelp");
               },
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
               leading: Icon(
                 Icons.help,
-                color: Colors.blue,
+                color: Colors.white,
               ),
               title: Text(
                 "مساعدة",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
             ),
             ListTile(
               onTap: () {},
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
               leading: Icon(
                 Icons.info,
-                color: Colors.blue,
+                color: Colors.white,
               ),
               title: Text(
                 "حول التطبيق",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
             ),
             ListTile(
               onTap: () {
                 Navigator.of(context).pushNamed("login");
               },
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
               leading: Icon(
                 Icons.person,
-                color: Colors.blue,
+                color: Colors.white,
               ),
               title: Text(
                 "تسجيل الدخول",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
             ),
-          ],
+          ]),
         ),
       ),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:app_idea/component/category_home.dart';
 import 'package:app_idea/component/drawer.dart';
+import 'package:app_idea/component/test.dart';
+import 'package:app_idea/pages/nature.dart';
 import 'package:app_idea/pages/start.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
@@ -35,19 +37,22 @@ class _HomeState extends State<Home> {
         ? Directionality(
             textDirection: TextDirection.rtl,
             child: Scaffold(
-                appBar: AppBar(
-                  backgroundColor: Colors.redAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.vertical(bottom: Radius.circular(15))),
-                  title: Text(
-                    "Idea",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                appBar: PreferredSize(
+                  preferredSize: Size.fromHeight(60.0),
+                  child: AppBar(
+                    backgroundColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.vertical(bottom: Radius.circular(15))),
+                    title: Text(
+                      "Idea",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    centerTitle: true,
+                    actions: <Widget>[
+                      IconButton(icon: Icon(Icons.search), onPressed: () {})
+                    ],
                   ),
-                  centerTitle: true,
-                  actions: <Widget>[
-                    IconButton(icon: Icon(Icons.search), onPressed: () {})
-                  ],
                 ),
                 drawer: MyDrawer(),
                 body: ListView(
@@ -101,8 +106,7 @@ class _HomeState extends State<Home> {
                       padding: EdgeInsets.all(10),
                       child: Text(
                         "الأقسام",
-                        style: TextStyle(
-                            fontSize: 30, color: Colors.redAccent[200]),
+                        style: TextStyle(fontSize: 30, color: Colors.redAccent),
                       ),
                     ),
                     Container(
@@ -111,17 +115,54 @@ class _HomeState extends State<Home> {
                         width: 200,
                         child: CategoriesHome()),
                     Container(
-                      height: 400,
-                      color: Color.fromRGBO(58, 66, 86, 1.0),
-                      child: GridView(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2),
-                        children: <Widget>[Text('data')],
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'أخر المنتجات',
+                        style: TextStyle(fontSize: 30, color: Colors.redAccent),
                       ),
-                    )
+                    ),
+                    Container(
+                      height: 400,
+                      child: GridView(
+                          padding: EdgeInsets.all(5),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                          children: <Widget>[
+                            buildCard('images/1.jpg', 'nature'),
+                            buildCard('images/2.jpg', 'nature'),
+                            buildCard('images/3.jpg', 'nature'),
+                            buildCard('images/1.jpg', 'nature'),
+                            buildCard('images/2.jpg', 'nature'),
+                            buildCard('images/3.jpg', 'nature'),
+                          ]),
+                    ),
                   ],
                 )),
           )
         : Start_Screen();
+  }
+
+  InkWell buildCard(String image, String category) {
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                child: Image.asset(
+              image,
+              fit: BoxFit.fill,
+            )),
+            Container(
+                child: Text(
+              category,
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            )),
+          ],
+        ),
+      ),
+    );
   }
 }
